915e0eafb86971a1c3dc653f118871adc8a799bd
Michael Hartl <michael@michaelhartl.com>
2011-05-06 16:23:19 -0700

    Heroku cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+    # Use the Heroku Varnish cache.
+    def heroku_cache
+      if Rails.env.production?
+        age = 1.day.to_i
+        response.headers['Cache-Control'] = "public, max-age=#{age}"
+      end
+    end
+
 end
--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,5 +1,7 @@
 class ChaptersController < ApplicationController
 
+  before_filter :heroku_cache
+
   def show
     chapter = params[:id]
     @content = File.open("public/book/#{chapter}_fragment.html").read

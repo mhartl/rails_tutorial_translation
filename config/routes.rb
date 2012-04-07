2aedc941ae613d609bef623074392b63fdf08e19
gafar <r125644@gmail.com>
2012-04-07 13:47:45 +0800

    Russian. Add Help page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 RailsTutorialTranslation::Application.routes.draw do
   resources :chapters, :only => ['show']
   match '/' => redirect('/chapters/beginning')
-
+  match '/help' => 'pages#help'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
